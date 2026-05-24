2ce5ed2a12327d3a3f1d48facf9a2976a2d24576
Michael L <michael@Michaels-MacBook-Air.local>
2026-05-24 18:52:46 +1000

    Shorten separator line

--- tracker.py
+++ tracker.py
@@ -221,7 +221,7 @@ def main():
         else:
             header = f"✈️ <b>Flight Tracker Update</b>\n"
 
-        msg  = "――――――――――――――――――――――――\n"
+        msg  = "―――――――――――――――――――\n"
         msg += header
         msg += f"🕐 Data pulled: {now_str}\n"
         msg += f"<b>{origin} → {destination}</b> ({trip_type}) | Depart {dep_date}\n\n"
